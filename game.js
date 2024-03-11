e14321c36e1ab13dd71a7bcd94238ef1dbdc3c92
ac1002 <13202081@life.hkbu.edu.hk>
2024-03-12 01:22:57 +0800

    Enhance game.js with user interaction and sound playback functionality
    This commit expands the game's interactive capabilities in game.js for the Sequence Master game. The updates include:
    
    - A `userClickedPattern` array to store the sequence of buttons that the user presses.
    - Modification to the `nextSequence` function to call a new function `playSound` with the chosen color.
    - A new `.click` event handler for the `.btn` class to:
       - Capture the id of the button that the user clicks.
       - Add the user's chosen color to the `userClickedPattern` array.
       - Log the user's sequence to the console.
       - Play the sound corresponding to the button clicked by the user.
    - The `playSound` function, which takes a color name and plays the associated sound file.
    
    These enhancements allow the game to respond to user inputs, maintain the sequence of user actions, and provide auditory feedback for each button press.

--- game.js
+++ game.js
@@ -2,16 +2,30 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 
+let userClickedPattern = [];
+
 function nextSequence() {
   const randomNumber = Math.floor(Math.random() * 4);
   console.log(randomNumber);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  //Show sequence to the user with flash animation and respective sound
+  //Show sequence to the user with flash animation and sound
   $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-  audio.play();
+  playSound(randomChosenColour);
 }
 nextSequence();
+
+//Click which button is pressed by user and add sounds to buttons clicks
+$(".btn").click(function () {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  playSound(userChosenColour);
+});
+
+function playSound(name) {
+  let audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
+}
