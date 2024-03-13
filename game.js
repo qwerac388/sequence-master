4b3e31c697f038c9b927531359c582106a6adac7
ac1002 <13202081@life.hkbu.edu.hk>
2024-03-13 15:17:27 +0800

    Add sequence checking logic and reset user pattern for new levels
    Enhanced the gameplay logic in `game.js` with the following updates:
    
    - Cleared `userClickedPattern` at the start of `nextSequence()` to ensure a fresh sequence for the user to follow each level.
    - Implemented `checkAnswer()` function to compare the user's input with the game pattern:
       - Checks if the most recent user input matches the corresponding element in the game pattern.
       - On success, if the entire sequence is correct and complete, it triggers the `nextSequence()` after a delay, advancing the game to the next level.
       - On failure, logs an error indicating the user has pressed the wrong button (further functionality to be implemented).
    
    This commit lays the groundwork for a robust sequence matching game where players are challenged to remember and replicate an increasingly complex series of inputs.

--- game.js
+++ game.js
@@ -5,12 +5,16 @@ let started = false;
 let level = 0;
 
 function nextSequence() {
+  userClickedPattern = [];
   level++;
   $("#level-title").text(`Level ${level}`);
+
+  //generate random number ranging from 1-4
   const randomNumber = Math.floor(Math.random() * 4);
   console.log(randomNumber);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+  console.log(gamePattern);
 
   //Show sequence to the user with flash animation and sound
   $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -22,11 +26,16 @@ function nextSequence() {
 $(".btn").click(function () {
   let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+
   console.log(userClickedPattern);
+  console.log(userClickedPattern.length);
+
   playSound(userChosenColour);
 
   //Add Animation to User Clicks
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function playSound(name) {
@@ -49,3 +58,17 @@ $(document).keypress(function () {
     started = true;
   }
 });
+
+//check if the most recent user answer is the same as game pattern and check if user finished their sequence
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+  }
+}
